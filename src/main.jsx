345ab55b75fc5c40af33d0b04f3b9088ba6c88e0
Jorge Parreira <jorgeparreira@MacBookPro.home>
2026-02-01 19:44:15 +0000

    chore: save current workspace state (move core logic, move App to src/web)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./web/App";
 import "./index.css"; // aqui é importante
 
 ReactDOM.createRoot(document.getElementById("root")).render(
